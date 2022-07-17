0cc8958cff4a401c7b49e26c692c96dbbf96f3e5
KimDongBeen <85830445+KimDongBeen@users.noreply.github.com>
2022-07-17 11:15:26 +0900

    Create 숫자 게임.cpp

---
+++ 숫자 게임.cpp
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12987
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    sort(A.begin(), A.end()); sort(B.begin(), B.end());
+    int index = 0;
+    for(int i = 0 ; i < A.size() ; i++){
+        for(int j = index ; j < B.size() ; j++){
+            if(A[i] < B[j]){
+                answer++;
+                index = j + 1;
+                break;
+            }
+        }
+    }
+    return answer;
+}
