748e34b361c236db26086ccf5b572c9727a1095e
KimDongBeen <85830445+KimDongBeen@users.noreply.github.com>
2022-09-10 00:12:13 +0900

    Create 징검다리.cpp

---
+++ 징검다리.cpp
@@ -0,0 +1,27 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/43236#
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+    int answer = 0;
+    rocks.push_back(0); rocks.push_back(distance);
+    sort(rocks.begin(), rocks.end());
+    int l = 0; int r = distance;
+    while(l + 1 < r){
+        int m = (l + r) / 2;
+        vector<int> vec = rocks;
+        int cnt = 0;
+        for(int i = 0 ; i < vec.size() - 1 ; i++){
+            if(vec[i + 1] - vec[i] < m){
+                ++cnt;
+                vec.erase(vec.begin() + i + 1);
+                i--;
+            }
+        }
+        if(cnt <= n){l = m; continue;}
+        else{r = m; continue;}
+    }
+    return l;
+}
