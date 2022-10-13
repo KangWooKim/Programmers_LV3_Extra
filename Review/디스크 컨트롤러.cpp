732dcffbdb55a16e899095d70e5813b7439f7fb6
KimDongBeen <85830445+KimDongBeen@users.noreply.github.com>
2022-10-13 16:56:14 +0900

    Update 디스크 컨트롤러.cpp

--- Review/디스크 컨트롤러.cpp
+++ Review/디스크 컨트롤러.cpp
@@ -1,34 +1,29 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42627
 
+#include <string>
+#include <vector>
 #include <bits/stdc++.h>
-
 using namespace std;
 
-bool compare(vector<int> a, vector<int> b){
-    if(a[0] == b[0]) return a[1] < b[1];
-    else return a[0] < b[0];
+bool cmp(vector<int> v1, vector<int> v2){
+    if(v1[0] == v2[0]) return v1[1] > v2[1];
+    return v1[0] < v2[0];
 }
 
 int solution(vector<vector<int>> jobs) {
-    int answer = 0; int work = jobs.size(); int time = 0;
-    sort(jobs.begin(), jobs.end(), compare);
-    while(jobs.size() != 0){
-        int len = 1001; int n = 0; int idx = 0;
-        if(jobs[0][0] > time){time++; continue;}
-        while(true){
-            if(idx > jobs.size() - 1 || jobs[idx][0] > time){
-                break;
-            }
-            if(len > jobs[idx][1]){
-                len = jobs[idx][1];
-                n = idx;
-            }
-            idx++;
+    int answer = 0; int j = jobs.size();
+    stable_sort(jobs.begin(), jobs.end(), cmp);
+    int time = 0;
+    while(!jobs.empty()){
+        int idx = 0; int m = 1001;
+        if(time < jobs[idx][0]) time = jobs[idx][0];
+        for(int i = 0 ; i < jobs.size() ; i++){
+            if(time < jobs[i][0]) break;
+            if(time >= jobs[i][0] && m > jobs[i][1]){idx = i; m = jobs[i][1];}
         }
-        time = time + jobs[n][1];
-        answer = answer + time - jobs[n][0];
-        jobs.erase(jobs.begin() + n);
+        time += jobs[idx][1];
+        answer += (time - jobs[idx][0]);
+        jobs.erase(jobs.begin() + idx);
     }
-
-    return answer / work;
+    return answer / j;
 }
