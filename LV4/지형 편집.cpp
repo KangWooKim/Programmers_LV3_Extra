6e036403bbf146c4940261df78260d986c094522
KimDongBeen <85830445+KimDongBeen@users.noreply.github.com>
2023-04-30 00:06:13 +0900

    Update 지형 편집.cpp

--- LV4/지형 편집.cpp
+++ LV4/지형 편집.cpp
@@ -1,2 +1,28 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12984
 
+#include<bits/stdc++.h>
+using namespace std;
+
+long long solution(vector<vector<int> > land, int P, int Q) {
+    long long answer = 9223372036854775807;
+    
+    vector<long long> block; long long total = 0;
+    for(auto& i : land){
+        for(auto& j : i)
+            {block.push_back(j);
+            total += j;}
+    }
+    stable_sort(block.begin(), block.end()); 
+    long long floor = -1; long long res = 0;
+    for(int i = 0 ; i < block.size() ; i++){
+        if(floor == block[i]) {res += block[i]; continue;}
+        floor = block[i];
+        long long plus = block[i] * i - res;
+        long long minus = total - res - block[i] * (block.size() - i);
+        long long cost = P * plus + Q * minus;
+        answer = min(answer, cost);
+        res += block[i];
+    }
+    
+    return answer;
+}
